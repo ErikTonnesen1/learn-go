fbf8705501ff7ca2f2f6bf04e0384347e52dea5f
Erik Tonnesen <eriktonnesen@Eriks-MacBook-Air.local>
2026-08-11 19:02:31 -0400

    greenlight - routes.go, movies.go

---
+++ lets-go-further/greenlight/cmd/api/helpers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"errors"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) readIDParam(r *http.Request) (int, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
