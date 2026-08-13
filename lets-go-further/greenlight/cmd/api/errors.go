51d85e7ddd614c9f5d4760aed96a2a6ece287d13
James Tonnesen <james.tonnesen@nextraq.com>
2026-08-13 17:02:54 -0400

    greenlight json error handling

---
+++ lets-go-further/greenlight/cmd/api/errors.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) logError(r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+// Helper for sending JSON-formatted error messages to client w/ a given status code
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+// Method used when encountering errors during runtime. Will log detailed error message, use the errorResponse() helper method to send a 500 response, containing a generic JSON response to the client
+func (app *application) serveErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
