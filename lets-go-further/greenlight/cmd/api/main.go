81483a95f5964f8833a2fb0f50501210038b2a3a
James Tonnesen <james.tonnesen@nextraq.com>
2026-08-20 16:26:57 -0400

    greenlight decouple DSN from hard-coded value to env var in zshrc

--- lets-go-further/greenlight/cmd/api/main.go
+++ lets-go-further/greenlight/cmd/api/main.go
@@ -34,7 +34,7 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (dev|stg|prod)")
 
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:paSSword@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 
 	flag.Parse()
 
